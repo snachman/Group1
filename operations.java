35f94cdc702fbd34522741cd4e2d20205041570e
Samuel Nachman <snachman@student.umuc.edu>
2020-04-21 19:32:30 -0400

    added test method

--- operations.java
+++ operations.java
@@ -1,7 +1,14 @@
 public class operations {
 
+
+
     public static String hello_world(){
         return "Hello Group";
+    }
+
+
+    public static int add_one(int x) {
+        return x + 1;
     }
 
 }
