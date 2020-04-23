cbb0cc07a7ec39cb7e151b0ec1c647d5871ceac8
snachman <shnachman@gmail.com>
2020-04-23 17:29:27 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
-package oncall;
+package offcall;
+
+
+
+
 import java.awt.Component;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
+import java.awt.GraphicsConfiguration;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -14,152 +20,142 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
 
-import java.awt.EventQueue;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 
-public class Main extends JFrame  {
 
-	private JLayeredPane contentPane;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Main frame = new Main();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the frame.
-	 */
+
+
+public class Main extends JPanel{
+
+	static JPanel ui = new JPanel();
+	static JRadioButton popButton = new JRadioButton("Pop");
+	static JRadioButton edmButton = new JRadioButton("Electronic");
+	static JRadioButton countryButton = new JRadioButton("Country");
+
+	static JButton searchButton = new JButton("Search");
 	
-	public Main() {
-		
-		handler h = new handler();				
-		
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 568, 420);
-		contentPane = new JLayeredPane();
-		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
-		setContentPane(contentPane);
+
+	
+	static JComboBox artistsBox = new JComboBox();
+
+	static String[] popArtists = { "a", "b", "c"};
+	static String[] edmArtists = { "d", "e", "f"};
+	static String[] countryArtists = { "g", "h", "i"};
+	
+	public static void genre_box_shower() {
+		popButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				// TODO Auto-generated method stub
+				artistsBox.setModel(new DefaultComboBoxModel(popArtists));
+			}
+			
+		});
+		edmButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				// TODO Auto-generated method stub
+				artistsBox.setModel(new DefaultComboBoxModel(edmArtists));
+			}
+			
+		});
+
+		countryButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				// TODO Auto-generated method stub
+				artistsBox.setModel(new DefaultComboBoxModel(countryArtists));
+			}
+			
+		});
+
+	
+	
+	
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		frame.setSize(600,300);  
+		genre_box_shower();
 		
 
-		
 		JLabel title = new JLabel("Music Artist Search");
 		title.setBounds(106, 5, 370, 27);
 		title.setAlignmentX(Component.CENTER_ALIGNMENT);
 		title.setFont(new Font("Lucida Grande", Font.BOLD, 34));
+
 		
 		JLabel header = new JLabel("Select a music genre to search");
 		header.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		header.setBounds(131, 66, 289, 27);
 		header.setVerticalAlignment(SwingConstants.BOTTOM);
+//
+//		
+		//radio buttons
+		ButtonGroup buttonGroup = new ButtonGroup();
 		
-		
-		JRadioButton popButton = new JRadioButton("Pop");
 		buttonGroup.add(popButton);
 		popButton.setBounds(164, 123, 55, 23);
-		popButton.addActionListener(h);
 		
-		
-		
-		
-		
-		JRadioButton edmButton = new JRadioButton("Electronic");
+
 		buttonGroup.add(edmButton);
 		edmButton.setBounds(231, 123, 94, 23);
-		edmButton.addActionListener(h);
 		
-		JRadioButton countryButton = new JRadioButton("Country");
 		buttonGroup.add(countryButton);
 		countryButton.setBounds(338, 122, 82, 23);
-		countryButton.addActionListener(h);
 		
 		
+		//edm_combo
 		
 		
-		JLabel header2 = new JLabel("Select a musical artist");
-		header2.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
-		header2.setBounds(182, 179, 214, 27);
+		artistsBox.setBounds(207, 236, 143, 27);
+		artistsBox.setEnabled(true);
+		artistsBox.setVisible(true);
+//
+//
+//		
+//		
+//		
+//		
+//		
+//
 		
 		
-		contentPane.setLayout(null);
-		contentPane.add(title);
-		contentPane.add(header);
-		contentPane.add(popButton);
-		contentPane.add(edmButton);
-		contentPane.add(countryButton);
-		contentPane.add(header2);
-//		contentPane.add(comboBox);
-
-		
-		JButton searchButton = new JButton("Search");
 		searchButton.setBounds(246, 335, 117, 29);
 		searchButton.setEnabled(true); //set back to false
-		contentPane.add(searchButton);
-		
-
-	}
-	
-	
-	
-	
-	
-class handler implements ActionListener{
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println(e.getActionCommand() + " selected");
-		JComboBox pop_combo;
-		JComboBox electronic_combo;
-//
-//		if (e.getActionCommand() == "Electronic") {
-//			String[] edmArtists = { "Skrillex", "Diplo", "Marshmello"};
-//			electronic_combo = new JComboBox(edmArtists);
-//			electronic_combo.setBounds(207, 236, 143, 27);
-//			electronic_combo.setVisible(true);
-//			electronic_combo.setEnabled(true);
-//			
-//			contentPane.add(electronic_combo);
-//
-//		}		
-
-		if (e.getActionCommand() == "Electronic") {
-			String[] popArtists = { "Skrillex", "Diplo", "Marshmello"};
-			electronic_combo = new JComboBox(popArtists);
-			electronic_combo.setBounds(207, 236, 143, 27);
-			electronic_combo.setEnabled(true);
-			electronic_combo.setVisible(true);
-			contentPane.add(electronic_combo);
-			contentPane.moveToFront(electronic_combo);
-			System.out.println("test");
-		}		
 
 		
-		else if (e.getActionCommand() == "Pop") {
-			String[] popArtists = { "Taylor Swift", "Pitbull", "Justin Bieber"};
-			pop_combo = new JComboBox(popArtists);
-			pop_combo.setBounds(207, 236, 143, 27);
-			pop_combo.setEnabled(true);
-			pop_combo.setVisible(true);
-			contentPane.add(pop_combo);
-			contentPane.moveToFront(pop_combo);
-			System.out.println("test");
-		}		
-//		else if (e.getActionCommand()== "Country") {
-//			System.out.println("test3");
-//		}
+		
+		
+		frame.setLayout(null);
+		frame.add(title);
+		frame.add(header);
+		frame.add(popButton);
+		frame.add(edmButton);
+		frame.add(countryButton);
+		frame.add(artistsBox);
+		frame.add(searchButton);
+		
+		frame.add(ui);
+		frame.setVisible(true);
 	
+		
 	}
-}
-//TODO: a
+
+
+	
+	
 }
